2cb495a82f7ca1ca011ffc5f51fec4841b8bf24d
Ruslan <yarema2003r@gmail.com>
2023-10-03 23:59:25 +0300

    ECR fixes

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -5,3 +5,24 @@ resource "aws_ecr_repository" "app_ecr_repo" {
 
 resource "aws_ecr_lifecycle_policy" "lanandra_ip_reader" {
   repository = aws_ecr_repository.app_ecr_repo.name
+
+
+  policy = jsonencode(
+  {
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+  })
+}
