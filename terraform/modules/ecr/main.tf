0ca491daa2c4f66d7314efcec8ea90598d8fee00
Ruslan <yarema2003r@gmail.com>
2023-10-03 23:58:25 +0300

    ECR fixes

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,3 +2,6 @@ resource "aws_ecr_repository" "app_ecr_repo" {
   name = "app-repo"
   force_delete = true
 }
+
+resource "aws_ecr_lifecycle_policy" "lanandra_ip_reader" {
+  repository = aws_ecr_repository.app_ecr_repo.name
