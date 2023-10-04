b2c8d005587a66ae066d8c5f82efd0afaade80d0
Ruslan <yarema2003r@gmail.com>
2023-10-04 22:18:12 +0300

    global infra update

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ module "ecr" {
 module "backend" {
   source = "./terraform/modules/backend"
 }
+
+module "ecs_task_definition" {
+  source = "./modules/ecs_task_definition"
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+  ecs_task_definition=module.ecs_task_definition.ecs_task_definition
+}
