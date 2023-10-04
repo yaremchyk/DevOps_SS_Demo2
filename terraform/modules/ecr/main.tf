d9bb2878494db9b9386fe15a3bdccce4cd531681
Ruslan <yarema2003r@gmail.com>
2023-10-04 18:55:42 +0300

    Network components added

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "app_ecr_repo" {
   name = "app-repo"
   force_delete = true
+  vpc_id = aws_vpc.default.id
+  subnet_id      = aws_subnet.private[count.index].id
 }
 
 resource "aws_ecr_lifecycle_policy" "lanandra_ip_reader" {
