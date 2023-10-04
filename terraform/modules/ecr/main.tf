d8d78f100f6ad3c98759c9a195a9ae81cd9a76dc
Ruslan <yarema2003r@gmail.com>
2023-10-04 19:43:16 +0300

    Network components added

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,75 +1,10 @@
 resource "aws_ecr_repository" "app_ecr_repo" {
-  name        = "app-repo"
+  name = "app-repo"
   force_delete = true
-
-  tags = {
-    Name = "${var.namespace}_ECRRepo_${var.environment}"
-  }
 }
-
-resource "aws_iam_policy" "ecr_policy" {
-  name        = "ecr-policy"
-  description = "ECR policy to allow access from specific VPC/subnet"
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetRepositoryPolicy",
-          "ecr:ListImages",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetLifecyclePolicy",
-          "ecr:GetLifecyclePolicyPreview",
-          "ecr:GetRepositoryPolicy",
-          "ecr:DescribeRepositories",
-          "ecr:GetRepositoryPolicy",
-          "ecr:CreateRepository",
-          "ecr:CompleteLayerUpload",
-          "ecr:UploadLayerPart",
-        ],
-        Effect = "Allow",
-        Resource = aws_ecr_repository.app_ecr_repo.arn,
-        Condition = {
-          StringEquals = {
-            "aws:sourceVpc": "vpc-xxxxxxxxxxxx"  # Specify your VPC ID here
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "ecr_role" {
-  name = "ecr-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecr.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "ecr_attachment" {
-  policy_arn = aws_iam_policy.ecr_policy.arn
-  roles      = [aws_iam_role.ecr_role.name]
-}
-
-resource "aws_ecr_repository_policy" "app_ecr_policy" {
-  repository = aws_ecr_repository.app_ecr_repo.name
-  policy     = aws_iam_policy.ecr_policy.policy
-}
-
 
 resource "aws_ecr_lifecycle_policy" "lanandra_ip_reader" {
+  repository = aws_ecr_repository.app_ecr_repo.name
   repository = aws_ecr_repository.app_ecr_repo.name
 
 
