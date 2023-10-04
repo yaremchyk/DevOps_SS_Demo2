21c49ac45a39ebc4f1486464e36d509ea2be3292
Ruslan <yarema2003r@gmail.com>
2023-10-04 19:36:12 +0300

    Network components added

---
+++ terraform/modules/backend/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket. Must be globally unique."
+  type        = string
+  default     = "trfm-ste-bkt-final"
+}
+
+variable "table_name" {
+  description = "The name of the DynamoDB table."
+  type        = string
+  default     = "terraform-lock"
+}
+
+variable "region" {
+    description = "The name of the region where we're working."
+    type        = string
+    default     = "us-east-2"
+}
