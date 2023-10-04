ed2c4e75d236980f3376bb53d523741659183ebe
Ruslan <yarema2003r@gmail.com>
2023-10-04 18:28:35 +0300

    Network components added

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -49,60 +49,8 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-
 resource "aws_main_route_table_association" "public_main" {
   vpc_id         = aws_vpc.default.id
-  route_table_id = aws_route_table.public.id
-}
-
-
-
-resource "aws_eip" "nat_gateway" {
-  count = var.az_count
-  vpc   = true
-
-  tags = {
-    Name     = "${var.namespace}_EIP_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gateway" {
-  count         = var.az_count
-  subnet_id     = aws_subnet.public[count.index].id
-  allocation_id = aws_eip.nat_gateway[count.index].id
-
-  tags = {
-    Name     = "${var.namespace}_NATGateway_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = var.az_count
-  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = aws_vpc.default.id
-
-  tags = {
-    Name     = "${var.namespace}_PrivateSubnet_${count.index}_${var.environment}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default.id
-  }
-
-  tags = {
-    Name     = "${var.namespace}_PublicRouteTable_${var.environment}"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = var.az_count
-  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
@@ -154,3 +102,4 @@ resource "aws_route_table_association" "private" {
   count          = var.az_count
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.*.id
+}
