74e98a1adfb564acadedc67b06bbab3bb27dddcc
Ruslan <yarema2003r@gmail.com>
2023-10-04 19:26:58 +0300

    Network components added

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -43,6 +43,19 @@ resource "aws_iam_policy" "ecr_policy" {
 
 resource "aws_iam_role" "ecr_role" {
   name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecr.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy_attachment" "ecr_attachment" {
